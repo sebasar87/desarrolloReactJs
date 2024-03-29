7a0ce7ea45e743c3344f7e2cdf4bdb22975de5b3
sebasar87 <arenas87sebastian@gmail.com>
2024-03-29 00:05:45 -0300

    se agrego el nav y se dieron estilos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,33 @@
-
 import logo from "../assets/img/logo.png"
 import "./navbar.css"
+import { BiCartDownload } from "react-icons/bi";
 function Navbar() {
     return (
-        <nav>
-            <div>
-                <img src={logo} className="logo" alt=''></img>
+        <nav className="navBar">
+            <div className="logo">
+                <img src={logo} className="logo-img" alt=''></img>
             </div>
-            <ul>
-                <li>
-                    <a href='#'>Inicio</a>
-                </li>
-                <li>
-                    <a href='#'>Mis compras</a>
-                </li>
-                <li>
-                    <a href='#'>Ofertas</a>
-                </li>
-                <li>
-                    <a href='#'>Nosotros</a>
-                </li>
-            </ul>
+            <div className="nav-div-list">
+                <ul className="nav-list">
+                    <li className="nav-list-li">
+                        <a href='#' target="_blank">Inicio</a>
+                    </li>
+                    <li className="nav-list-li">
+                        <a href='#' target="_blank">Mis compras</a>
+                    </li>
+                    <li className="nav-list-li">
+                        <a href='#' target="_blank">Ofertas</a>
+                    </li>
+                    <li className="nav-list-li">
+                        <a href='#' target="_blank">Nosotros</a>
+                    </li>
+                </ul>
+                <div className="nav-div-carrito">
+                    <BiCartDownload className="nav-carrito" size="30px" />
+                    <span className="nav-span">0</span>
+                </div>
+            </div>
+            
         </nav>
     )
 }
