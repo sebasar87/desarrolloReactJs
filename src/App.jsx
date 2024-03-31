53b2754ef1fa338c7efe31a006544363186ba2b3
sebasar87 <arenas87sebastian@gmail.com>
2024-03-30 21:40:43 -0300

    se agregor saludo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar"
+import Myitems from "./components/Myitems"
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Myitems greeting={"Hola, bienvenido al mundo del mañana "}/>
     </>
   )
 }
---
+++ src/components/Myitems.jsx
@@ -0,0 +1,9 @@
+function items({greeting}) {
+  return (
+    <div>
+        <h1>{greeting} USUARIO</h1>
+    </div>
+  )
+}
+
+export default items
